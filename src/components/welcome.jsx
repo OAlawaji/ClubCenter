a311bb476eef94897786eec57c73b9085739c220
OAlawaji <s202025780@kfupm.edu.sa>
2024-05-08 16:50:00 +0300

    added animations

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,17 +1,39 @@
 import React from "react";
+// import { CSSTransition } from 'react-transition-group';
+
 import imageSrc from '../assets/KFUPMtower.png';
 function Welcome() {
+    // const [imageInProp, setImageInProp] = useState(false);
+    // const [textInProp, setTextInProp] = useState(false);
+  
+    // useEffect(() => {
+    //   setImageInProp(true);
+    //   setTimeout(() => {
+    //     setTextInProp(true);
+    //   }, 2000); // same as image animation duration
+    // }, []);
+
     return (
         <div className="max-w-[1248px] w-full flex flex-row">
-       <img src={imageSrc} className="sm:block hidden" alt="image" />  
+       <img src={imageSrc} className="sm:block 
+       hidden
+       logo
+       " alt="image" />  
         <div className="flex flex-col 
               justify-center items-start       
-
+              
+              welcome
               font-almarai font-bold text-sakry
               space-y-4"
               id="home"
               >
-                <h1 className="text-4xl sm:text-4xl font-extrabold ">Welcome to KFUPM Club Center</h1> 
+                <h1 className="text-4xl 
+                sm:text-4xl 
+                font-extrabold 
+                ">
+                    Welcome to KFUPM Club Center
+                </h1> 
+                
                 <p>Join where you can find Clubs or Activities that matches your interests <br/>
                 Or create your own Club or Activity if you are authorized by signing in </p> 
                 <button className="px-4 md:px-4 py-2 md:py-2 rounded-xl font-xl
